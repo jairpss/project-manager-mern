d8c46fdc94190df29c8c1375e135c5befb9d6b93
Jair Partida <jairpartida1@gmail.com>
2022-11-21 21:06:28 -0700

    Create user Routing

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
+import userRoutes from "./routes/userRoutes.js"
 
 const app = express()
 
 dotenv.config()
 
 connectDB()
+
+//Routing
+app.use("/api/users", userRoutes)
 
 const PORT = process.env.PORT || 4000
 
